d9520082fecd84a0eb6c903917a987928af18ca0
AndreiPancu <andreipancu95@gmail.com>
2020-09-14 19:02:11 +0300

    Add Room database + Entities for Category and Label

---
+++ app/src/main/java/com/example/budgetapp/repository/local/LabelDao.kt
@@ -0,0 +1,21 @@
+package com.example.budgetapp.repository.local
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.budgetapp.repository.model.Label
+
+interface LabelDao {
+
+    @Query("SELECT * FROM label")
+    fun getAll(): List<Label>
+
+    @Query("SELECT * FROM label WHERE name LIKE :name LIMIT 1")
+    fun findLabelByName(name: String): Label
+
+    @Insert
+    fun insertAll(vararg categories: Label)
+
+    @Delete
+    fun delete(category: Label)
+}
--- app/src/main/java/com/example/budgetapp/repository/model/Label.kt
+++ app/src/main/java/com/example/budgetapp/repository/model/Label.kt
@@ -1,6 +1,12 @@
 package com.example.budgetapp.repository.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "label")
 data class Label(
-    val name: String,
-    val amount: Double
+    @PrimaryKey val labelId: Int,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "amount") val amount: Double
 )
