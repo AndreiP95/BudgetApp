621ec184f6eb3514a14113a1679bf598a9c94134
AndreiPancu <andreipancu95@gmail.com>
2020-09-18 23:15:49 +0300

    Added documentation template

--- app/src/main/java/com/example/budgetapp/repository/model/Label.kt
+++ app/src/main/java/com/example/budgetapp/repository/model/Label.kt
@@ -3,6 +3,15 @@ package com.example.budgetapp.repository.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+/**
+ * Table for Category Labelsa
+ * @property labelId Long
+ * @property categoryNameId Long
+ * @property name String
+ * @property amount Double
+ * @constructor
+ */
+
 @Entity(tableName = "label")
 data class Label(
     @PrimaryKey val labelId: Long,
